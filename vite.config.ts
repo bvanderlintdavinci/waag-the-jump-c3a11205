29bce86647a74c1c7d0f94e148af4fa51d7516f1
Piekaboe <bvanderlint@gmail.com>
2026-08-21 20:53:06 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,14 +5,4 @@ export default defineConfig({
     // Redirect TanStack Start's bundled server entry to src/server.ts
     server: { entry: "server" },
   },
-  vite: {
-    // Dwing Nitro om statische bestanden te bouwen voor GitHub Pages i.p.v. Cloudflare
-    nitro: {
-      preset: "github-pages",
-      prerender: {
-        routes: ["/"],
-        crawlLinks: true,
-      },
-    },
-  },
 });
